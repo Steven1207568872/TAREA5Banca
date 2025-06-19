ed1fc86fda1064fd0872d915f1e6a7bebab66619
Steven1207568872 <strivinol@uteq.edu.ec>
2025-06-19 11:43:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center">
+    <!-- Primera imagen al 50% -->
+    <img src="https://github.com/user-attachments/assets/cc9ed5e3-0fd3-4e02-83a9-c081ecd8d471" alt="Screenshot 2" width="25%">
+    
+</p>
---
+++ app/src/main/java/com/example/bancapichincha/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bancapichincha
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.illustrationImage)
+        Glide.with(this).asGif().load(R.drawable.caminando).into(imageView)
+
+    }
+}
